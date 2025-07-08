fc6b3633512b6b00e48be2e5c97b526ea1dc7f79
Arpit Vashi <arpitvashi17@gmail.com>
2025-07-08 20:59:05 +0530

    Income Tax Calculator

---
+++ incometaxcalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class incometaxcalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your income: ");
+        int income = sc.nextInt();
+        if (income < 0) {
+            System.out.println("Income cannot be negative.");
+        } else {
+            double tax = calculateTax(income);
+            System.out.printf("Income Tax would be: %.2f\n", tax);
+        }
+        sc.close();
+    }
+
+    private static double calculateTax(int income) {
+        double tax = 0;
+        if (income > 800000) {
+            tax = income * 0.10;
+        } else if (income > 200000) {
+            tax = income * 0.07;
+        } else if (income > 0) {
+            tax = income * 0.05;
+        } else {
+            tax = 0;
+        }
+        return tax;
+    }
+}
