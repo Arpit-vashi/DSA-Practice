67ce46fb7764c6c1598f008114051d7dc27af6da
Arpit Vashi <arpitvashi17@gmail.com>
2025-07-09 20:43:57 +0530

    Take user input and print if number is even or odd

--- numberisevenorodd.java
+++ numberisevenorodd.java
@@ -1,15 +1,29 @@
+//Write a Java program that determines if a user-input number is even or odd
 import java.util.Scanner;
 
 public class numberisevenorodd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        int n = sc.nextInt();
-    if (n % 2 == 0) {
-        System.out.println("even");
-    }
-    else {
-        System.out.println("odd");
-    }
+
+        try {
+            System.out.print("Enter an integer: ");
+
+            if (!sc.hasNextInt()) {
+                throw new IllegalArgumentException("Invalid input. Please enter an integer.");
+            }
+
+            int n = sc.nextInt();
+
+            if (n % 2 == 0) {
+                System.out.println(n + " is even");
+            } else {
+                System.out.println(n + " is odd");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
     }
 }
